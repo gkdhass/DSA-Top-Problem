66eac0aa03580b9b10d3741f3facdd0747f3c60c
Mohan Dhass G <mohandhassgkvsb@gmail.com>
2026-08-06 10:23:40 +0530

    Time: 1 ms (97.68%), Space: 42.7 MB (43.43%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            long s = (long) mid * mid;
+
+            if( s == x){
+                return mid;
+            }
+            else if( s < x){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+}
